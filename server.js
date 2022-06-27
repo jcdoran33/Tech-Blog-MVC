70d5c833863ab42f9861291fa6a530628b74a809
Jack Doran <jcdoran33@gmail.com>
2022-06-27 17:37:49 -0400

    added server.js file and some psuedo code

---
+++ server.js
@@ -0,0 +1,16 @@
+//layout imports/requirements
+
+
+
+//define const routes >controllers folder|| sequelize > config/connection||helpers? utils/helpers
+
+
+//define app as express();
+const app = express();
+//define PORT as process.envPORT || 3001
+const PORT = process.env.PORT || 3001;
+
+//define sess (session) as an object with secret:, cookie:, store: etc
+
+//app.use(session(sess));
+
