970223f97c3c4edfe0369c8e5a2e32102c8b8456
Jack Doran <jcdoran33@gmail.com>
2022-06-28 21:55:48 -0400

    added new routing to ndex.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,13 @@
 //import express, and any__routes files used in API folder
 const router = require('express').Router();
+const commentRoutes = require("./comment-routes");
+const postRoutes = require("./post-routes");
+const userRoutes = require("./user-routes");
 
-
-
+//app.use....
+router.use("/comments", commentRoutes);
+router.use("/posts", postRoutes);
+router.user("/users", userRoutes);
 
 
 //export router
--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,13 @@ const router = require("express").Router();
 
 //define apiRoutes - any others?
 const apiRoutes = require("./api");
-const homeRoutes = require("./home-routes.js")
+const homeRoutes = require("./home-routes.js");
+const dashboardRoutes = require("./dashboard-routes");
 
 //router.use "/", "/api"
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 //export router
 module.exports = router;
